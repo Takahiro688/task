00916334f06a47a63a54f6673f8e5bf44af8eb01
Takahiro688 <75800455+Takahiro688@users.noreply.github.com>
2023-06-05 05:13:36 +0900

    Add files via upload

---
+++ htdocs/page-contact.php
@@ -0,0 +1,40 @@
+
+    <!--お問い合わせフォーム-->
+   
+
+        <table class="contact-form">
+        <tr>
+            <th><p>お問合せ種別</p><p>必須</p></th>
+            <td><label><input type="radio" name="select" value="お問い合せ" checked="checked" /><span class="">お問い合せ</span></label></td>
+        </tr>
+
+        <tr>
+            <th><p>お名前</p><p>必須</p></th>
+            <td><input type="text" name="your-name" value="" size="40" class="" aria-required="true" aria-invalid="false" placeholder="例）田中太郎" required/></td>
+        </tr>
+
+        <tr>
+            <th><p>フリガナ</p><p>必須</p></th>
+            <td><input type="text" name="your-furigana" value="" size="40" class="" aria-required="true" aria-invalid="false" placeholder="例）田中太郎" required/></td>
+        </tr>
+
+        <tr>
+            <th><p>電話番号</p></th>
+            <td><input type="tel" name="your-tel" value="" size="40" class="" aria-invalid="false" placeholder="例）090-0000-0000" required /></td>
+        </tr>
+
+        <tr>
+            <th><p>メールアドレス</p><p>必須</p></th>
+            <td><input type="email" name="your-email" value="" size="40" class="" aria-required="true" aria-invalid="false" placeholder="例）sample@torat.jp" required/></td>
+        </tr>
+
+        <tr>
+            <th><p>お問い合わせ内容</p><p>必須</p></th>
+            <td><textarea name="your-message" cols="40" rows="10" class="" aria-required="true" aria-invalid="false" placeholder="お問い合わせの入力欄" required></textarea></td>
+        </tr>
+
+        </table>
+
+<input type="submit" value="送信する" class="" id="formbtn" /></p>
+    </form>
+
